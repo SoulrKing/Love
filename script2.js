f0df2b6971e7426e20ffae3e67d511df457c8b10
Roger Henrique <conteudosroger@gmail.com>
2025-07-01 16:36:19 -0300

    finalizado

---
+++ script2.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('bg-video');
+    if (video) {
+        video.playbackRate = 0.8; // 0.5 = metade da velocidade normal
+    }
+});
